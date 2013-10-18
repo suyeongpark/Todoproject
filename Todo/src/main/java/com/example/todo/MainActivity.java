8c3bae418afb90f71ec584066de2213e068dff21
suyeongpark <suyeongpark@abyne.com>
2013-10-18 12:23:49 +0900

    첫 번째 스터디한 내용 커밋.  * todo 추가하기

--- Todo/src/main/java/com/example/todo/MainActivity.java
+++ Todo/src/main/java/com/example/todo/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.todo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,21 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    public void mOnClick(View v) {
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+
+        TextView textView;
+
+        int resID = getResources().getIdentifier(String.format("textView0%d", count), "id", getPackageName());
+        textView = (TextView) findViewById(resID);
+        count++;
+        if ( count > 3 )
+            count = 1;
+
+        textView.setText(editText.getText());
+        editText.setText("");
+    }
+
 }
