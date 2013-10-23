2ed64e2ee7f8fa12080a0e3ade2770247a141f36
suyeongpark <suyeongpark@abyne.com>
2013-10-23 14:32:39 +0900

    이벤트 상속 받아서  구현하는 거.

--- Todo/src/main/java/com/example/todo/MainActivity.java
+++ Todo/src/main/java/com/example/todo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.todo;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +26,28 @@ public class MainActivity extends Activity implements View.OnClickListener {
 //        }
 //    }
 
+    class MyTextView extends TextView {
+
+        public MyTextView(Context context, Editable text) {
+            super(context);
+
+            setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            setBackgroundColor(Color.parseColor("#00FFFFFF"));
+            setPadding(20, 10, 10, 10);
+            setTextColor(Color.parseColor("#FF7200"));
+            setTextSize(13);
+
+            setText(text);
+        }
+
+        public boolean onTouchEvent(android.view.MotionEvent event) {
+            Toast.makeText(getContext(), "My Text View", Toast.LENGTH_LONG).show();
+
+            return false;
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +60,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
         View.OnClickListener a = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ((MainActivity)view.getContext()).onClick(view);
                 Toast.makeText(view.getContext(), "clicked", Toast.LENGTH_LONG).show();
             }
         };
 
-        button.setOnClickListener(a);
+        button.setOnClickListener( a );
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,17 +78,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         EditText editText = (EditText)findViewById(R.id.editText);
-        TextView textView = new TextView(this);
-
-        //ScrollView scrollView = (ScrollView)findViewById(R.id.scroll);
-
-        textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        textView.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-        textView.setPadding(20, 10, 10, 10);
-        textView.setTextColor(Color.parseColor("#FF7200"));
-        textView.setTextSize(13);
-
-        textView.setText(editText.getText());
+        TextView textView = new MyTextView(this, editText.getText());
 
         LinearLayout topLL = (LinearLayout)findViewById(R.id.todoListLayout);
         topLL.addView(textView, 0);
