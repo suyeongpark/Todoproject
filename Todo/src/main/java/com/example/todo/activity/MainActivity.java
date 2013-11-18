3a3918f2918825d757d2231a8a1cb05c5c6886ad
neomparam <neomparam@gmail.com>
2013-11-18 23:41:50 +0900

    새 todo 생성시 ListView 갱신.

--- Todo/src/main/java/com/example/todo/activity/MainActivity.java
+++ Todo/src/main/java/com/example/todo/activity/MainActivity.java
@@ -4,14 +4,10 @@ package com.example.todo.activity;
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.DragEvent;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,19 +15,20 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.todo.R;
-import com.example.todo.db.LocalDB;
 import com.example.todo.db.PersistantModel;
 import com.example.todo.view.TodoItemView;
 
-public class MainActivity extends Activity implements View.OnDragListener {
+public class MainActivity extends Activity {
     private static final String LOG_TAG = "MainActivity";
 
     private LinearLayout mainLayout;
-//    private ViewGroup _layout_todolist;
     private ListView _list_todo;
     private Spinner _spinner_priority;
+
+    private SimpleCursorAdapter _cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,26 +55,15 @@ public class MainActivity extends Activity implements View.OnDragListener {
 
         PersistantModel.initialize(this);
 
-//        _layout_todolist = (ViewGroup)findViewById(R.id.todoListLayout);
-//        _layout_todolist.setOnDragListener(this);
-
-//        loadToDoList();
-
-
-
         String[] fromColumns = {"content"};
         int[] toViews = {R.id.textView};
 
-        SQLiteDatabase db;
-        db = new LocalDB(this).getWritableDatabase();
+        SQLiteDatabase db = PersistantModel.getDb();
         Cursor cursor = db.rawQuery("SELECT * FROM db_user ORDER BY _id", null);
-
-        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.todo_item, cursor, fromColumns, toViews, 0);
-
-//        ArrayAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray);
+        _cursorAdapter = new SimpleCursorAdapter(this, R.layout.todo_item, cursor, fromColumns, toViews, 0);
 
         _list_todo = (ListView)findViewById(R.id.list_todo);
-        _list_todo.setAdapter(cursorAdapter);
+        _list_todo.setAdapter(_cursorAdapter);
     }
 
     @Override
@@ -87,24 +73,11 @@ public class MainActivity extends Activity implements View.OnDragListener {
         return true;
     }
 
-    private void loadToDoList() {
-        Cursor c = PersistantModel.getItems();
-        c.moveToFirst();
-
-        for ( int i = 0 ; i < c.getCount(); i++ ) {
-            addTodoItem(c.getInt(0), c.getString(1), c.getInt(2));
-            c.moveToNext();
-        }
-    }
-
-    private void addTodoItem(int id, String text, int priority) {
-        View todoItem = new TodoItemView(this, id, text, priority).getView();
-//        todoItem.setOnDragListener(this);
-
-//        _layout_todolist.addView(todoItem, 0);
-    }
-
     public void deleteTodoItem(int id, TodoItemView item) {
+        Cursor newCursor = PersistantModel.getDb().rawQuery("SELECT * FROM db_user ORDER BY _id", null);
+        _cursorAdapter.changeCursor(newCursor);
+        _cursorAdapter.notifyDataSetChanged();
+
 //        _layout_todolist.removeView(item.getView());
     }
 
@@ -128,68 +101,14 @@ public class MainActivity extends Activity implements View.OnDragListener {
                 break;
         }
 
-        int id = PersistantModel.createItem(text, priority);
-        addTodoItem(id, text, priority);
+        PersistantModel.createItem(text, priority);
 
         editText.setText("");
-    }
-/*
-    private int calcDropIndex(DragEvent dragEvent) {
-//        Log.i("DragAction", "  x = " + dragEvent.getX());
-//        Log.i("DragAction", "  y = " + dragEvent.getY());
 
-//        Log.i("DragAction", "  container.y = " + _layout_todolist.getY());
-//        Log.i("DragAction", "  container.scrollY = " + _layout_todolist.getScrollY());
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
 
-        float eventY = dragEvent.getY();
-        int sumHeight = 0;
-        for ( int i = 0 ; i < _layout_todolist.getChildCount() ; i++ )
-        {
-            int childHeight = _layout_todolist.getChildAt(i).getHeight();
-            if ( eventY >= sumHeight && eventY < sumHeight + childHeight )
-                return i;
-
-            sumHeight += childHeight;
-        }
-
-        return 0;
-    }
-*/
-    @Override
-    public boolean onDrag(View view, DragEvent dragEvent) {
-//        int action = dragEvent.getAction();
-//        View src = (View)dragEvent.getLocalState();
-//        Drawable enterShape = getResources().getDrawable(R.drawable.shape_droptarget);
-//        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
-//        int index;
-//
-//        switch ( action ) {
-//            case DragEvent.ACTION_DRAG_STARTED:
-//                break;
-//            case DragEvent.ACTION_DRAG_LOCATION:
-//                index = calcDropIndex(dragEvent);
-//                Log.i("DragAction", "  index = " + index);
-//                Log.i("DragAction", _layout_todolist.getChildAt(index).toString());
-//                break;
-//            case DragEvent.ACTION_DROP:
-//                index = calcDropIndex(dragEvent);
-//                _layout_todolist.removeView(src);
-//                _layout_todolist.addView(src, index);
-//                src.setVisibility(View.VISIBLE);
-//                break;
-//            case DragEvent.ACTION_DRAG_ENDED:
-//                if ( !dragEvent.getResult() )
-//                    src.setVisibility(View.VISIBLE);
-//                break;
-//            case DragEvent.ACTION_DRAG_ENTERED:
-//                break;
-//            case DragEvent.ACTION_DRAG_EXITED:
-//                break;
-//            default:
-//                return true;
-//        }
-//
-//        return true;
-        return false;
+        Cursor newCursor = PersistantModel.getDb().rawQuery("SELECT * FROM db_user ORDER BY _id", null);
+        _cursorAdapter.changeCursor(newCursor);
+        _cursorAdapter.notifyDataSetChanged();
     }
 }
--- Todo/src/main/java/com/example/todo/db/PersistantModel.java
+++ Todo/src/main/java/com/example/todo/db/PersistantModel.java
@@ -8,29 +8,32 @@ import android.database.sqlite.SQLiteDatabase;
  * Created by bayja on 2013. 10. 29..
  */
 public class PersistantModel {
-    private static SQLiteDatabase db;
+    private static SQLiteDatabase _db;
 //    private static Context context;
 
     static {
     }
 
     public static void initialize(Context context) {
-//        PersistantModel.context = context;
-        db = new LocalDB(context).getWritableDatabase();
+        _db = new LocalDB(context).getWritableDatabase();
     }
 
     public static Cursor getItems() {
-        return db.rawQuery("SELECT _id, content, priority FROM db_user ORDER BY priority DESC", null);
+        return _db.rawQuery("SELECT _id, content, priority FROM db_user ORDER BY priority DESC", null);
     }
 
     public static int createItem(String text, int priority) {
-        db.execSQL("INSERT INTO db_user (content, priority) VALUES ('" + text + "', " + priority + ");");
-        Cursor c = db.rawQuery("SELECT _id FROM db_user ORDER BY _id DESC LIMIT 1", null);
+        _db.execSQL("INSERT INTO db_user (content, priority) VALUES ('" + text + "', " + priority + ");");
+        Cursor c = _db.rawQuery("SELECT _id FROM db_user ORDER BY _id DESC LIMIT 1", null);
         c.moveToFirst();
         return c.getInt(0);
     }
 
     public static void deleteItem(int id) {
-        db.execSQL("DELETE FROM db_user WHERE _id = " + id);
+        _db.execSQL("DELETE FROM db_user WHERE _id = " + id);
+    }
+
+    public static SQLiteDatabase getDb() {
+        return _db;
     }
 }
